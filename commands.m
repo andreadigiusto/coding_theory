1b4915ba9d72e639695c85e8608ee796e09fed36
andreadigiusto <andigiusto@gmail.com>
2024-07-25 10:34:43 +0200

    automated search part 1
    commands for automated seqrch

--- commands.m
+++ commands.m
@@ -5,3 +5,23 @@ readfile := function(N)
 end function;
 
 //PrintFileMagma("namefile.m" , print_obj : Overwrite:=true); prints file WITH overwrite
+
+load "mu_examples.m";
+data := readfile("examples_[8,4]7.m");
+Mlist := readfile("sub_lists_7.m");
+codes := data[1];
+
+G := RandomMatrix(GF(7),4,8);
+C := LinearCode(G);
+D := Dual(C);
+for i in [1..#data[1]] do
+    if IsEquivalent(C,data[1,i]) or IsEquivalent(D,data[1,i]) then
+        i;
+        break;
+    end if;
+end for;
+Clist := all_sub(C);
+Dlist := all_sub(D);
+mu_lookup(Clist,Mlist);
+mu_lookup(Dlist,Mlist);
+Append(~data[1],C);
