0291bb26bdf5c4f5447ac54a35d981ce2023daf0
andreadigiusto <andigiusto@gmail.com>
2024-07-29 09:18:37 +0200

    at home
    more experiments

--- commands.m
+++ commands.m
@@ -1,16 +1,18 @@
+//one-time commands
+
 readfile := function(N)
     s := Read(N);
     OB := eval s;
     return OB;
 end function;
 
-//PrintFileMagma("namefile.m" , print_obj : Overwrite:=true); prints file WITH overwrite
-
 load "mu_examples.m";
-data := readfile("examples_[8,4]7.m");
 Mlist := readfile("sub_lists_7.m");
-codes := data[1];
 
+//iterable commands
+
+data := readfile("examples_[8,4]7.m");
+codes := data[1];
 G := RandomMatrix(GF(7),4,8);
 C := LinearCode(G);
 D := Dual(C);
@@ -25,3 +27,5 @@ Dlist := all_sub(D);
 mu_lookup(Clist,Mlist);
 mu_lookup(Dlist,Mlist);
 Append(~data[1],C);
+
+//PrintFileMagma("namefile.m" , print_obj : Overwrite:=true); prints file WITH overwrite
